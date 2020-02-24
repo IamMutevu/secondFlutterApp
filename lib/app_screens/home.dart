e6b550f646debec48ff81d38eea826c7b98b556a
Alabee <ianmutevu96@gmail.com>
2020-02-24 17:15:52 +0300

    Basic setup

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: Text(
+            "Flight",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 75.0,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+              color: Colors.white
+            ),
+        ),
+      )
+    );
+
+  }
+}
