f16d4b7bd7982c55e0f776f5ca7ebf26be895019
Alabee <ianmutevu96@gmail.com>
2020-02-25 12:40:14 +0300

    Button and alert dialog box

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget{
       child: Container(
         padding: EdgeInsets.only(left: 10.0, top: 40.0, right: 10.0),
         alignment: Alignment.center,
-        color: Colors.deepPurple,
+        color: Colors.white,
         child: Column(
           children: <Widget>[
             Row(
@@ -22,7 +22,7 @@ class Home extends StatelessWidget{
                         fontSize: 35.0,
                         fontFamily: 'Raleway',
                         fontWeight: FontWeight.w700,
-                        color: Colors.white
+                        color: Colors.blueAccent
                     ),
                   ),
                 ),
@@ -35,7 +35,7 @@ class Home extends StatelessWidget{
                         fontSize: 20.0,
                         fontFamily: 'Raleway',
                         fontWeight: FontWeight.w700,
-                        color: Colors.white
+                        color: Colors.blueAccent
                     ),
                   ),
                 ),
@@ -52,7 +52,7 @@ class Home extends StatelessWidget{
                         fontSize: 35.0,
                         fontFamily: 'Raleway',
                         fontWeight: FontWeight.w700,
-                        color: Colors.white
+                        color: Colors.blueAccent
                     ),
                   ),
                 ),
@@ -65,13 +65,14 @@ class Home extends StatelessWidget{
                         fontSize: 20.0,
                         fontFamily: 'Raleway',
                         fontWeight: FontWeight.w700,
-                        color: Colors.white
+                        color: Colors.blueAccent
                     ),
                   ),
                 ),
               ],
             ),
-            FlightImageAsset()
+            FlightImageAsset(),
+            FlightBookButton()
           ],
         )
       )
@@ -84,9 +85,50 @@ class FlightImageAsset extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/plane.jpg');
-    Image image = Image(image: assetImage, width: 400.0, height: 400.0,);
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
 
     return Container(child: image);
   }
 
 }
+
+class FlightBookButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      height: 50.0,
+      width: 250.0,
+      child: RaisedButton(
+        color: Colors.indigoAccent,
+        child: Text(
+            "Book Your Flight",
+          style: TextStyle(
+            fontFamily: "Raleway",
+            fontSize: 20.0,
+            color: Colors.white,
+            fontWeight: FontWeight.w700
+          ),
+        ),
+        elevation: 6.0,
+        onPressed: (){
+          bookFlight(context);
+          }),
+        );
+      }
+    }
+
+    void bookFlight(BuildContext context){
+      var alertDialog = AlertDialog(
+        title: Text("Flight booked successfully"),
+        content: Text("Have a pleasant flight"),
+      );
+      
+      showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return alertDialog;
+        }
+      );
+    }
